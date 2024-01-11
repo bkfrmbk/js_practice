1a6182a28fba6677536f85037256d9618ca47584
Bradley Korman <bk@bradleys-air.mynetworksettings.com>
2024-01-11 13:23:38 -0500

    test commit

---
+++ Modules/01_variable_dataType/helloWorld.js
@@ -0,0 +1,18 @@
+// Comments!
+
+console.log('Hello world');
+
+let myAge = 32;
+let isDoorOpen = true;
+
+let person = {
+    firstName : 'bred',
+    lastName : 'kroman',
+    age : 33
+};
+
+let cities = [ 'new york', 'london', 'paris' ];
+
+
+console.log(typeof cities);
+console.log(cities);

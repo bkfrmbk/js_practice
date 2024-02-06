f655111cf9708937b1339cae2ebede6b4888a3a4
Bradley Korman <bk@bradleys-air.mynetworksettings.com>
2024-02-05 22:05:32 -0500

    first code challenge

---
+++ Modules/class1/CodeChallenge.js
@@ -0,0 +1,52 @@
+// Write a program that declares a variable name and assigns it a string value of your first name. 
+
+const myName = 'Brad Korman';
+
+// Then, print the value of the name variable to the console.
+
+console.log(myName);
+
+// Write a program that declares a variable age and assigns it a number value of your age. 
+
+var myAge = 32;
+
+// Then, print the value of the age variable to the console.
+
+console.log(myAge);
+
+// Write a program that declares a variable isStudent and assigns it a boolean value of 
+// true or false depending on whether or not you are currently a student. 
+
+var isStudent = true;
+
+// Then, print the value of the isStudent variable to the console.
+
+console.log(isStudent);
+
+// Write a program that declares a variable favoriteColors and assigns it an array of string values representing your favorite colors. 
+
+var favoriteColors = ['Green', 'Blue', 'Purple'];
+
+// Then, print the value of the favoriteColors variable to the console.
+
+console.log(favoriteColors);
+
+// Write a program that declares a variable person and assigns it an 
+// object with properties for name, age, and isStudent. 
+
+let person = {
+    name : '',
+    age : 0,
+    isStudent : false
+}
+console.log(person);
+
+// Assign values to each property based on your own personal information. 
+
+person.name = myName;
+person.age = myAge;
+person.isStudent = true;
+
+// Then, print the value of the person variable to the console.
+
+console.log(person);
